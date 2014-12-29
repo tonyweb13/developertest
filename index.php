bfb4a5644744e82fbe67baa7f61f16830ed8cfa9
Anthony Juntila <anthony.juntila@ph-resources.com>
2014-12-29 17:17:38 +0800

    revise some codes in klyp exam

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-
 $apikey = 'cqkqdys7rzvkwawfzn762wkn';
 
-$q = urlencode('red'); // make sure to url encode an query parameters
+// make sure to url encode an query parameters
+$q = urlencode('red');
+
 // construct the query with our apikey and the query we want to make
 $endpoint = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' . $apikey . '&q=' . $q;
-
-
+//'&g='. $g. '&b='. $b. '&y='. $y;
 // setup curl to make a call to the endpoint
 $session = curl_init($endpoint);
 
@@ -26,40 +26,53 @@ if ($search_results === NULL)
     die('Error parsing json');
 
 // play with the data!
+
 $movies = $search_results->movies;
+?>
 
-echo '<ul>';
+<ul>
 
+<?php
 $i = 0;
 $len = count($movies);
 
 foreach ($movies as $movie) {
 
     if ($i == 0) {
-        echo '<div style="border:1px solid black; margin:10px 10px 10px 0; padding:10px; width:350px; background:red;">';
-        echo '<img src="' . $movie->posters->profile . '" />';
-        echo '<a href="' . $movie->links->alternate . '" style="color:white; margin-left:10px; position:absolute; font-size:25px; font-style:strong;" >' . $movie->title . '</a>';
-        echo '<br>';
-        echo '<br>';
-        echo '<p style="color:white; margin-left:62px; position:absolute;" >';
-        echo 'Year: ' . $movie->year;
-        echo '<br>';
-        echo 'Run Time: ' . $movie->runtime . ' min.';
-        echo '<br>';
-        echo 'Rating: ' . $movie->mpaa_rating;
-        echo '</p>';
+        ?>
 
-        echo '<h3 style="color:white;">Description:</h3>';
-        echo '<p style="text-align:justify; color:white;">' . $movie->synopsis . '</p>';
-        echo '</div>';
-        echo '<br>';
-    } else {
+            <div style="border:1px solid black; margin:10px 10px 10px 0; padding:10px; width:350px; background:red;">
+                <img src="<?php echo $movie->posters->profile; ?>" />
+                <a href="' . $movie->links->alternate . '" style="color:white; margin-left:10px;  position:absolute; font-size:25px; font-style:strong;" >
+        <?php echo $movie->title; ?> 
+                </a>
+                <br>
+                <br>
+                <p style="color:white; margin-left:62px; position:absolute;" >
+                    Year: <?php echo $movie->year; ?>
+                    <br>
+                    Run Time: <?php echo $movie->runtime; ?> min
+                    <br>
+                    Rating: <?php echo $movie->mpaa_rating; ?>
+                </p>
+                <h3 style="color:white;">Description:</h3>
+                <p style="text-align:justify; color:white;">
+        <?php echo $movie->synopsis; ?>
+                </p>
+            </div>
+            <br>
 
-        echo '<li><a href="' . $movie->links->alternate . '">' . $movie->title . " (" . $movie->year . ") - " . $movie->runtime . " min.</a></li>";
-    }
+    <?php } else { ?>
 
-    $i++;
-}
+            <li><a href="<?php echo $movie->links->alternate; ?> ">
+            <?php echo $movie->title; ?> ( <?php echo $movie->year; ?>) 
+                    -
+                    <?php echo $movie->runtime; ?> min.</a></li>
+                <?php
+                }
 
-echo '</ul>';
-?>
+                $i++;
+            }
+            ?>
+
+</ul>
